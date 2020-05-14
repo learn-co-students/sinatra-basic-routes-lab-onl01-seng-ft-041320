3905a07474258a003d697f50ee8d51355e68f0e3
Xearta <rsmcwow@gmail.com>
2020-05-14 00:49:10 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Tanner"
+  end
+
+  get '/hometown' do
+    "My hometown is Collegeville"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Blank Space"
+  end
 end
