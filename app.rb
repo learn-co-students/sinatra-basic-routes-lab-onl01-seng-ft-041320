0d988bf27eb71217da4fd0247bdbc1f72817a923
webdevjscoder <cobianjuan19@gmail.com>
2020-05-18 10:09:32 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Juan"
+    end
+
+    get '/hometown' do
+        "My hometown is Whittier, CA." 
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Chemicals by Gregory Alan Isakov"
+    end
 end
