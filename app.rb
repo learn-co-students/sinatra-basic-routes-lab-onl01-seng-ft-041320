18f6d478273a764af826c6e24a51e71ea18e4043
Vent2 <ventura.c.gerald@gmail.com>
2020-05-22 19:39:04 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello World!"
+    end
+
+    get '/name' do
+        "My name is "
+    end
+
+    get '/hometown' do
+        "My hometown is Union City"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Crashing"
+    end
 end
