404c667519496f84394823d3bb9d84c0db21513d
DrewDalmedo <andrewdalmedo@gmail.com>
2020-05-24 18:51:22 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, world!"
+    end
+
+    get '/name' do
+        "My name is Drew"
+    end
+
+    get '/hometown' do
+        "My hometown is Jefferson"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Castle on a Hill"
+    end
 end
