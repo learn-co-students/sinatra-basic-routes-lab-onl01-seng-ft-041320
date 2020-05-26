23f75ac60b951a309ffcf90fcbe7f9490b01357a
codeweb123 <jiyonj@hotmail.com>
2020-05-25 23:55:04 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        'My name is Jiyon'
+    end
+
+    get '/hometown' do
+        'My hometown is Malabar,FL'
+    end
+
+    get '/favorite-song' do
+        'My favorite song is My Heart Will Go On'
+    end
 end
